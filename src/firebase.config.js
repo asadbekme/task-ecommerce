4e2cade889fb3a62ca7645eead925ae49617b654
Asadbek_Rakhimov <asadbekme2002@gmail.com>
2023-03-04 21:35:02 +0300

    firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// * Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChsSPslp9HdvAvAN5pZhwpPv8yyEjGwJQ",
+  authDomain: "e-commerce-aa180.firebaseapp.com",
+  projectId: "e-commerce-aa180",
+  storageBucket: "e-commerce-aa180.appspot.com",
+  messagingSenderId: "789963881424",
+  appId: "1:789963881424:web:34439f08bf455b9fda57d5",
+};
+
+// * Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
