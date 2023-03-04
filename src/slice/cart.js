c289e776b2f687513aa47728d0bf66f44290852f
Asadbek_Rakhimov <asadbekme2002@gmail.com>
2023-03-04 14:17:48 +0300

    Test

--- src/slice/cart.js
+++ src/slice/cart.js
@@ -3,16 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   cartProducts: [],
   totalAmount: 0,
-  totalQuantity: 0
+  totalQuantity: 0,
 };
 
 export const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     addProduct: (state, action) => {
       const newProduct = action.payload;
-      const existingProduct = state.cartProducts.find((product) => product.id === newProduct.id);
+      const existingProduct = state.cartProducts.find(
+        (product) => product.id === newProduct.id
+      );
       state.totalQuantity += 1;
       if (!existingProduct) {
         state.cartProducts.push({
@@ -21,14 +23,19 @@ export const cartSlice = createSlice({
           imgUrl: newProduct.imgUrl,
           price: newProduct.price,
           quantity: 1,
-          totalPrice: newProduct.price
+          totalPrice: newProduct.price,
         });
       } else {
         existingProduct.quantity += 1;
-        existingProduct.totalPrice = Number(existingProduct.totalPrice) + Number(newProduct.price);
+        existingProduct.totalPrice =
+          Number(existingProduct.totalPrice) + Number(newProduct.price);
       }
 
-      state.totalAmount = state.cartProducts.reduce((total, product) => total + Number(product.price) * Number(product.quantity), 0);
+      state.totalAmount = state.cartProducts.reduce(
+        (total, product) =>
+          total + Number(product.price) * Number(product.quantity),
+        0
+      );
       console.log(state.totalQuantity);
       console.log(state.totalAmount);
       console.log(newProduct);
@@ -36,15 +43,23 @@ export const cartSlice = createSlice({
 
     deleteProduct: (state, action) => {
       const id = action.payload;
-      const existingProduct = state.cartProducts.find((product) => product.id === id);
+      const existingProduct = state.cartProducts.find(
+        (product) => product.id === id
+      );
       if (existingProduct) {
-        state.cartProducts = state.cartProducts.filter((product) => product.id !== id);
+        state.cartProducts = state.cartProducts.filter(
+          (product) => product.id !== id
+        );
         state.totalQuantity = state.totalQuantity - existingProduct.quantity;
       }
 
-      state.totalAmount = state.cartProducts.reduce((total, product) => total + Number(product.price) * Number(product.quantity), 0);
-    }
-  }
+      state.totalAmount = state.cartProducts.reduce(
+        (total, product) =>
+          total + Number(product.price) * Number(product.quantity),
+        0
+      );
+    },
+  },
 });
 
 export const { addProduct, deleteProduct } = cartSlice.actions;
