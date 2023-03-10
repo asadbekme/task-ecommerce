221f40b8f588c8ad47621009aa811b3bc0880bd7
Asadbek_Rakhimov <asadbekme2002@gmail.com>
2023-03-11 00:21:46 +0500

    Admin Dashboard component

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,6 +1,44 @@
+import { Container, Row, Col } from "reactstrap";
+import "../styles/dashboard.scss";
+import useGetData from "../hooks/useGetData";
+
 const Dashboard = () => {
+  const { data: products } = useGetData("products");
+  const { data: users } = useGetData("users");
+
   return (
-    <div>Dashboard</div>
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col className="lg-3">
+              <div className="revenue__box">
+                <h5 className="box__title">Total Sales</h5>
+                <span className="box__value">$7890</span>
+              </div>
+            </Col>
+            <Col className="lg-3">
+              <div className="orders__box">
+                <h5 className="box__title">Orders</h5>
+                <span className="box__value">789</span>
+              </div>
+            </Col>
+            <Col className="lg-3">
+              <div className="products__box">
+                <h5 className="box__title">Total Products</h5>
+                <span className="box__value">{products.length}</span>
+              </div>
+            </Col>
+            <Col className="lg-3">
+              <div className="users__box">
+                <h5 className="box__title">Total Users</h5>
+                <span className="box__value">{users.length}</span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 };
 
